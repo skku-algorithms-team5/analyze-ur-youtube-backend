844d97eb5754b8ccadf60b57d8f10fcd3886f40a
Gyunseo Lee <rbstj0311@naver.com>
2023-12-04 17:57:02 +0900

    hotfix(be): change test_stream code

--- test_stream.py
+++ test_stream.py
@@ -1,9 +1,13 @@
 import requests
 
-localhost_url = "http://localhost:8000/stream_analyze/?url=https://www.youtube.com/watch?v=tnSUTDcKhPU"
-deploy_url = "https://port-0-analyze-ur-youtube-backend-3szcb0g2blp9bp0ek.sel5.cloudtype.app/stream_analyze/?url=https://www.youtube.com/watch?v=tnSUTDcKhPU"
-mocking_url = "http://localhost:8000/mocking_stream_analyze"
-with requests.get(mocking_url, stream=True) as r:
+localhost_url = "http://localhost:8000"
+deploy_url = (
+    "https://port-0-analyze-ur-youtube-backend-3szcb0g2blp9bp0ek.sel5.cloudtype.app"
+)
+stream_analyze_api = "/stream_analyze/?url=https://www.youtube.com/watch?v=tnSUTDcKhPU"
+mocking_stream_analyze_api = "/mocking_stream_analyze"
+
+with requests.get(deploy_url + mocking_stream_analyze_api, stream=True) as r:
     for chunk in r.iter_content(None, decode_unicode=True):
         if chunk:
             print(chunk, end="", flush=True)
