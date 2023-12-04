d332ab7d6de60cd2df4f5edd626038f5c3115d16
Gyunseo Lee <rbstj0311@naver.com>
2023-12-04 15:45:24 +0900

    test(be): sse test script에 localhost, deploy uri 구분

--- test_stream.py
+++ test_stream.py
@@ -1,8 +1,8 @@
 import requests
 
-url = "http://localhost:8000/stream_analyze/?url=https://www.youtube.com/watch?v=tnSUTDcKhPU"
-
-with requests.get(url, stream=True) as r:
+localhost_test_url = "http://localhost:8000/stream_analyze/?url=https://www.youtube.com/watch?v=tnSUTDcKhPU"
+deploy_test_url = "https://port-0-analyze-ur-youtube-backend-3szcb0g2blp9bp0ek.sel5.cloudtype.app/stream_analyze/?url=https://www.youtube.com/watch?v=tnSUTDcKhPU"
+with requests.get(localhost_test_url, stream=True) as r:
     for chunk in r.iter_content(None, decode_unicode=True):
         if chunk:
             print(chunk, end="", flush=True)
