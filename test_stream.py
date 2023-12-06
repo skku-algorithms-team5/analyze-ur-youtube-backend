c81daceafe6f5be583a12c7f1f58b53287985422
Gyunseo Lee <rbstj0311@naver.com>
2023-12-06 16:14:17 +0900

    hotfix(be): test_py script 수정

--- test_stream.py
+++ test_stream.py
@@ -7,7 +7,7 @@ deploy_url = (
 stream_analyze_api = "/stream_analyze?url=https://www.youtube.com/watch?v=tnSUTDcKhPU"
 mocking_stream_analyze_api = "/mocking_stream_analyze"
 
-with requests.get(deploy_url + mocking_stream_analyze_api, stream=True) as r:
+with requests.get(localhost_url + stream_analyze_api, stream=True) as r:
     for chunk in r.iter_content(None, decode_unicode=True):
         if chunk:
             print(chunk, end="", flush=True)
